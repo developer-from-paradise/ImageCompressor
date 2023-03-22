2bf00d075114fb8f7b110980d9d62dc352efdccf
Ismoil <ismailismailov8942@gmail.com>
2023-03-22 17:19:25 +0000

    Added new path for Windows

--- script.php
+++ script.php
@@ -6,9 +6,11 @@ require_once('imgCompress/Images.php');
 // Checking is file was uploaded into a server
 if(isset($_FILES['file'])){
     // Initializing path for uploaded file for webpage
-    $PATH = "localhost".str_replace(basename(__FILE__), '', $_SERVER['REQUEST_URI']).'upload/';
+    $localPATH = str_replace("\\", "/", $_SERVER['REQUEST_URI']); // For windows path
+    $PATH = "localhost".str_replace(basename(__FILE__), '', $localPATH).'upload/';
     // Initializing path for uploaded file for server
-    $PATH = getcwd().'/upload/';
+    $anotherPATH = str_replace("\\", "/", getcwd()); // For windows path
+    $PATH = $anotherPATH.'/upload/';
     // Creating object and setting pararmeters
     $image = new ImageEditor($_FILES['file'], 10000000, intval($_POST['quility']), $PATH);
     // Compressed file name
